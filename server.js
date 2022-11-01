ac9bb358a999c4573374cf8130eac93825e9b1fb
Andriele Aschidamini <andriele.aschidamini@pmweb.com.br>
2022-10-31 21:41:35 -0300

    preparação para publicação

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const { resolve } = require("path");
 
 const app = express();
 
-app.use("/", express.static(resolve(__dirname, "./build")));
+app.use(express.static(resolve(__dirname, "./dist")));
 
-app.listen(process.env.PORT || 3000, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-});
+app.listen(process.env.PORT || 5000);
+
+console.log("server started", port);
